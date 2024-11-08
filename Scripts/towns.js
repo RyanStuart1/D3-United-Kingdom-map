e6ef3a5592bd9ca0d3a6901c0065f4cda89d0127
Ryan <ryanstuart911@gmail.com>
2024-11-08 16:34:56 +0000

    commit - slider works, fixing tooltip

--- Scripts/towns.js
+++ Scripts/towns.js
@@ -4,36 +4,30 @@ var svg;
 var projection;
 
 function d3Draw(dataset){
-height = 1000;
-width = 1000;
-
-/* I used https://stackoverflow.com/questions/42055874/switching-projection-to-d3-geomercator-causes-errors 
-to help create var projection */
-
+    height = 1000;
+    width = 1000;
     const scalefactor = 0.01;
 
     projection = d3.geoMercator()
         .center([-4, 55])
         .scale(2590)
-        .translate([width / 2, height / 2]);
+        .translate([width / 2, height / 2]); // I used https://stackoverflow.com/questions/42055874/switching-projection-to-d3-geomercator-causes-errors to help create var projection
+        
 
-    if ((typeof svg == "undefined")){
+    if (!svg){
         svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height)
         .style("top", 100)
         .style("centre", 0)
         .style("z-index", 10)
-        .style("pointer-events", "none")
-        .style("position", "absolute");
-    } else {
-
-    }
+        .style("position", "absolute")
+    } 
 
 /* I used https://stackoverflow.com/questions/33899497/projectionlat-lng-keeps-returning-null
 to help return both longitude and latitude */
 
-    const town_circles = svg.selectAll("circle")
+    svg.selectAll("circle")
         .data(dataset)
         .enter()
         .append("circle")
@@ -48,19 +42,49 @@ to help return both longitude and latitude */
         })
         .attr("fill", "#f9098f")
         .style("stroke","black")
-        .style("stroke-width", 2);
+        .style("stroke-width", 2)
+        .on("mouseover", function(event, d) {
+            d3.select(".tooltiptext")
+                .transition()
+                .duration(200)
+                .style("opacity", 1) 
+                .text(`Town: ${d.Town}, County: ${d.County}, Population: ${d.Population}`); // Set text in tooltip
+        })
+        .on("mousemove", function(event) {
+            d3.select(".tooltip")
+                .style("left", (event.pageX + 10) + "px") 
+                .style("top", (event.pageY + 10) + "px");
+        })
+        .on("mouseout", function() {
+            d3.select(".tooltip")
+                .transition()
+                .duration(200)
+                .style("opacity", 0);
+        });
 }
 
 function d3Update(dataset){
     const scalefactor = 0.01;
 
     const town_circles = svg.selectAll("circle")
-        .data(dataset)
-        .transition()
-        .duration(2500)
-        .ease(d3.easeElastic)
+        .data(dataset, function(d) {
+            return d.id;
+        });
 
-        town_circles.attr("cx", function(d){
+    town_circles.transition()
+        .duration(0)
+        //.ease(d3.easeElastic)
+        .attr("cx", function(d) { 
+            return projection([d.lng, d.lat])[0]; })
+        .attr("cy", function(d) { 
+            return projection([d.lng, d.lat])[1]; })
+        .attr("r", function(d) { 
+            return Math.sqrt(d.Population) * scalefactor; 
+        });
+
+    town_circles.enter()
+        .append("circle")
+        .attr("cx", function(d){
             return projection([d.lng, d.lat])[0];
         })
         .attr("cy", function(d){
@@ -71,31 +95,47 @@ function d3Update(dataset){
         })
         .attr("fill", "#f9098f")
         .style("stroke","black")
-        .style("stroke-width", 2);
+        .style("stroke-width", 2.5)
+        //.transition()
+        //.duration(1000)
+        //.ease(d3.easeElastic);
+
+    town_circles.exit()
+        .transition()
+        .duration(1000)
+        .attr("r", 0) // Reduce radius to 0 for smooth removal
+        .remove(); // Remove the element from the DOM after transition
 }
 
 function loadData(){
+    const slider = document.getElementById("myRange");
+    const sliderValueDisplay = document.getElementById("slider-value");
+    sliderValueDisplay.textContent = slider.value;
 
-    d3.select("button")
+    updateData();
+
+    d3.select(".update-dataset")
     .on("click", function(){
+        sliderValueDisplay.textContent = slider.value;
         updateData();
-    });
-    d3.json("http://34.147.162.172/Circles/Towns/500",function(error, data){
-        if (error){
-            console.log(error)
-        }else{
-            d3Draw(data);
-        }
-    });   
+    }, {passive: true});
 }
 
-function updateData(){
+function updateData(){   
 
-    d3.json("http://34.147.162.172/Circles/Towns/500", function(error, data){
+    let sliderValue = document.getElementById("myRange").value;
+    document.getElementById("slider-value").textContent = sliderValue;
+    let url = `http://34.147.162.172/Circles/Towns/${sliderValue}`;
+
+    d3.json(url, function(error, data){
         if (error){
-            console.log(error)
-        }else{
-            d3Update(data);
+            console.log(error);
+        } else {
+            if (!svg){
+                d3Draw(data);
+            } else {
+                d3Update(data);
+            }
         }
     });
 }
